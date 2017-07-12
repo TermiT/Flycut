702777827104388476944e7df0b6233f2761daf0
Mark Jerde <mark.a.jerde@gmail.com>
2017-07-12 08:43:18 -0500

    Add FlycutOperator files to project, to be used to divide AppController into two files, one for UI and one for management of clipping stores.

---
+++ FlycutOperator.h
@@ -0,0 +1,13 @@
+//
+//  FlycutOperator.h
+//  Flycut
+//
+//  Created by Mark Jerde on 7/10/17.
+//
+//
+
+#ifndef FlycutOperator_h
+#define FlycutOperator_h
+
+
+#endif /* FlycutOperator_h */
---
+++ FlycutOperator.m
@@ -0,0 +1,9 @@
+//
+//  FlycutOperator.m
+//  Flycut
+//
+//  Created by Mark Jerde on 7/10/17.
+//
+//
+
+#import <Foundation/Foundation.h>
