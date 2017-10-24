d1502bc3bfac942c21a8d40700b2bfa46fcf3ed8
Mark Jerde <mark.a.jerde@gmail.com>
2017-10-24 14:58:04 -0500

    Import InAppSettingsKit 0ea1b98eb8ea3293845b86dfa0358ccd4d11e65f to provide the settings panel within the app.  This makes the settings easier to find for users who are not familiar with the iOS Settings.app approach and allows Flycut to provide user feedback in cases like shrinking the stack immediately when the setting is changed rather than a delayed cause-effect which would appear clunky.

---
+++ InAppSettingsKit/InAppSettingsKitSampleApp/CustomSubviewController.m
@@ -0,0 +1,35 @@
+//
+//  CustomSubviewController.m
+//  InAppSettingsKitSampleApp
+//  http://www.inappsettingskit.com
+//
+//  Copyright (c) 2009-2010:
+//  Luc Vandal, Edovia Inc., http://www.edovia.com
+//  Ortwin Gentz, FutureTap GmbH, http://www.futuretap.com
+//  All rights reserved.
+// 
+//  It is appreciated but not required that you give credit to Luc Vandal and Ortwin Gentz, 
+//  as the original authors of this code. You can give credit in a blog post, a tweet or on 
+//  a info page of your app. Also, the original authors appreciate letting them know if you use this code.
+//
+//  This code is licensed under the BSD license that is available at: http://www.opensource.org/licenses/bsd-license.php
+//
+
+#import "CustomSubviewController.h"
+#ifdef USES_IASK_STATIC_LIBRARY
+#import "InAppSettingsKit/IASKSpecifier.h"
+#else
+#import "IASKSpecifier.h"
+#endif
+
+
+@implementation CustomSubviewController
+
+- (id)initWithFile:(NSString*)file specifier:(IASKSpecifier*)specifier {
+	if ((self = [super init])) {
+		// custom initialization
+	}
+	return self;
+}
+
+@end
---
+++ InAppSettingsKit/InAppSettingsKitTests/IASKAbstractStoreTests.m
@@ -0,0 +1,103 @@
+//
+//  IASKAbstractStoreTests.m
+//  InAppSettingsKit
+//
+//  Created by Stephan Diederich on 19.12.12.
+//  Copyright (c) 2012 InAppSettingsKit. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import "IASKSettingsStore.h"
+
+@interface CustomStore : IASKAbstractSettingsStore { }
+
+@property (nonatomic, strong) id lastValue;
+@property (nonatomic, strong) NSString* lastKey;
+
+@end
+
+@implementation CustomStore
+
+- (void)setObject:(id)value forKey:(NSString *)key {
+    self.lastKey = key;
+    self.lastValue = value;
+}
+
+- (id)objectForKey:(NSString *)key {
+    self.lastKey = key;
+    return nil;
+}
+
+@end
+
+
+@interface IASKAbstractStoreTests : XCTestCase {
+    CustomStore* store;
+}
+@end
+
+
+@implementation IASKAbstractStoreTests
+- (void)setUp {
+    [super setUp];
+    
+    store = [CustomStore new];
+}
+
+- (void)tearDown {
+    store = nil;
+    
+    [super tearDown];
+}
+
+-(void)testAbstractStoreCallsSetObjectForBool {
+    [store setBool:YES forKey:@"MyKey"];
+    
+    XCTAssertEqualObjects(@"MyKey", store.lastKey, @"Key not used or set");
+    XCTAssertEqualObjects([NSNumber numberWithBool:YES], store.lastValue, @"Value not set");
+}
+
+-(void)testAbstractStoreCallsSetObjectForFloat {
+    [store setFloat:.12f forKey:@"MyKey1"];
+    
+    XCTAssertEqualObjects(@"MyKey1", store.lastKey, @"Key not used or set");
+    XCTAssertEqualWithAccuracy(.12f, [store.lastValue floatValue], .001f, @"Value not set");
+}
+
+-(void)testAbstractStoreCallsSetObjectForInteger {
+    [store setInteger:23 forKey:@"MyKey"];
+    
+    XCTAssertEqualObjects(@"MyKey", store.lastKey, @"Key not used or set");
+    XCTAssertEqual(23, [store.lastValue integerValue], @"Value not set");
+}
+
+-(void)testAbstractStoreCallsSetObjectForDouble {
+    [store setDouble:23. forKey:@"MyKey"];
+    
+    XCTAssertEqualObjects(@"MyKey", store.lastKey, @"Key not used or set");
+    XCTAssertEqualWithAccuracy(23., [store.lastValue doubleValue], .001, @"Value not set");
+}
+
+-(void)testAbstractStoreCallsGetObjectForDouble {
+    XCTAssertNil(store.lastKey, @"Should be nil");
+    [store doubleForKey:@"MyKey"];
+    XCTAssertEqual(store.lastKey, @"MyKey", @"objectforKey not called");
+}
+
+-(void)testAbstractStoreCallsGetObjectForFloat {
+    XCTAssertNil(store.lastKey, @"Should be nil");
+    [store floatForKey:@"MyKey"];
+    XCTAssertEqual(store.lastKey, @"MyKey", @"objectforKey not called");
+}
+-(void)testAbstractStoreCallsGetObjectForInteger {
+    XCTAssertNil(store.lastKey, @"Should be nil");
+    [store integerForKey:@"MyKey"];
+    XCTAssertEqual(store.lastKey, @"MyKey", @"objectforKey not called");
+}
+-(void)testAbstractStoreCallsGetObjectForBool {
+    XCTAssertNil(store.lastKey, @"Should be nil");
+    [store boolForKey:@"MyKey"];
+    XCTAssertEqual(store.lastKey, @"MyKey", @"objectforKey not called");
+}
+
+@end
