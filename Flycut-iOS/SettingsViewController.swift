dc7b728eac6f60b3cc71d4ed89f40fabaf0cafe5
Mark Jerde <mark.a.jerde@gmail.com>
2017-11-12 23:20:21 -0600

    Add Diagnostics to in-app settings.  Move reading of acknowledgements file to constructor of the SettingsViewController to delay execution until possibly needed.

--- Flycut-iOS/SettingsViewController.swift
+++ Flycut-iOS/SettingsViewController.swift
@@ -11,12 +11,23 @@ import Foundation
 class SettingsViewController: IASKAppSettingsViewController {
 	required init?(coder aDecoder: NSCoder) {
 		super.init(style: .grouped)
-		super.showCreditsFooter = false
+		commonInitContent()
 	}
 
 	// - (id)initWithStyle:(UITableViewStyle)style {
 	required override init(style:UITableViewStyle) {
 		super.init(style: style)
+		commonInitContent()
+	}
+
+	func commonInitContent() {
 		super.showCreditsFooter = false
+
+		let fileRoot = Bundle.main.path(forResource: "acknowledgements", ofType: "txt")
+		let contents = try? String.init(contentsOfFile: fileRoot!, encoding: String.Encoding.utf8)
+		UserDefaults.standard.set(contents, forKey: "acknowledgementsText")
+
+		let data = MJCloudKitUserDefaultsSync.diagnosticData()
+		UserDefaults.standard.set(data, forKey: "diagnosticsText")
 	}
 }
