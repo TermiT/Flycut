0129887c5f2a13e7c080ce245ea5171b7974efb6
Mark Jerde <mark.a.jerde@gmail.com>
2022-01-15 06:39:43 -0600

    Get Flycut iOS compiling and updated to Swift 5

--- Flycut-iOS/AppDelegate.swift
+++ Flycut-iOS/AppDelegate.swift
@@ -14,7 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
 
 	var window: UIWindow?
 
-	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 		// Override point for customization after application launch.
 		if #available(iOS 10.0, *) {
 			let center = UNUserNotificationCenter.current()
